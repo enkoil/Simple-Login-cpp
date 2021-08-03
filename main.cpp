9f98a5c0c3101771806013bd49d13d636749df7b
Nikole Varhegyi <varhegyin@gmail.com>
2021-08-03 17:20:30 -0400

    add isLoggedIn function

--- main.cpp
+++ main.cpp
@@ -8,3 +8,21 @@ simple file based login system based on: https://www.youtube.com/watch?v=I_aWPGC
 
 using namespace std;
 
+bool isLoggedIn() {
+    string username, password, un, pw;
+
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+
+    ifstream read("data\\" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password) {
+        return true;
+    } else {
+        return false;
+    }
+}
