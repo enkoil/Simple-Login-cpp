772a1911f5ea3e9b92cd0ba8e8905b609975efca
Nikole Varhegyi <varhegyin@gmail.com>
2021-08-04 01:15:24 -0400

    add caesarCipher function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ simple file based login system based on: https://www.youtube.com/watch?v=I_aWPGC
 
 using namespace std;
 
+const int CHAR_COUNT = 94; // ASCII characters 33 - 126 will be considered valid in a password
+const int SHIFT = 5;       // hard-coded Caesar cipher shift
+const int ASCII_MIN = 33; // smallest ASCII character considered valid in a password
+const string TEST_STRING = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
 bool isLoggedIn() {
     string username, password, un, pw;
 
@@ -25,6 +30,20 @@ bool isLoggedIn() {
     } else {
         return false;
     }
+}
+
+string caesarCipher(string word, int shift, bool encrypt) {
+    string result = "";
+    for (int i = 0; i < word.length(); i++){
+        if (encrypt){
+            // shift to range 0..94, apply cipher, shift to range 33..126
+            result += static_cast<char>((((static_cast<int>(word[i]) - ASCII_MIN) + SHIFT) % CHAR_COUNT) + ASCII_MIN);
+        } else {
+            // shift to range 0..94, apply cipher (accounting for negative numbers), shift to range 33..126
+            result += static_cast<char>(((((static_cast<int>(word[i]) - ASCII_MIN) - SHIFT) % CHAR_COUNT + CHAR_COUNT) % CHAR_COUNT) + ASCII_MIN);
+        }
+    }
+    return result;
 }
 
 int main() {
